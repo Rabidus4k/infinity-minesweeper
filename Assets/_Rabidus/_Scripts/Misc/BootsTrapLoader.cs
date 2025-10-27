ab038ac05733d7ecf2ddc8111eff79e5bf62fa8b
Rabidus <38137224+Rabidus4k@users.noreply.github.com>
2025-10-27 08:44:30 +0300

    bug fixes + added bootstrap

---
+++ Assets/_Rabidus/_Scripts/Misc/BootsTrapLoader.cs
@@ -0,0 +1,13 @@
+using MirraGames.SDK;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootsTrapLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        MirraSDK.WaitForProviders(static () => {
+            SceneManager.LoadScene("GameScene");
+        });
+    }
+}
